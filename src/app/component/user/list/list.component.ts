6d2b2e54e1945a1e0be487c37b7afcb9bd470909
AT User01 <atuser01@amp.local>
2019-01-30 22:34:31 +0530

    Ag Grid sample

--- src/app/component/user/list/list.component.ts
+++ src/app/component/user/list/list.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+  columnDefs = [
+        {headerName: 'Make', field: 'make'},
+        {headerName: 'Model', field: 'model'},
+        {headerName: 'Price', field: 'price'}
+    ];
+
+    rowData = [
+        {make: 'Toyota', model: 'Celica', price: 35000},
+        {make: 'Ford', model: 'Mondeo', price: 32000},
+        {make: 'Porsche', model: 'Boxter', price: 72000}
+    ];
 
   constructor() { }
 
--- src/app/component/user/user.module.ts
+++ src/app/component/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ListComponent } from './list/list.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    AgGridModule.withComponents(null)
   ],
   declarations: [ListComponent]
 })
