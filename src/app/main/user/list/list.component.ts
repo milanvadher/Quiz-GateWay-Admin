0afd09354a60edfe041bfa4dd1ebe494ade2c2b9
Milan Vadher <milanvadher1996@gmail.com>
2019-04-03 16:49:56 +0530

    User Listing in Material Data table

--- src/app/main/user/list/list.component.ts
+++ src/app/main/user/list/list.component.ts
@@ -1,127 +1,229 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from 'app/api.service';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { merge, Observable, BehaviorSubject, fromEvent, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+import { fuseAnimations } from '@fuse/animations';
+import { FuseUtils } from '@fuse/utils';
+
+import { takeUntil } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  animations: fuseAnimations
 })
 export class ListComponent implements OnInit {
 
-  private gridApi;
-  private gridColumnApi;
-  private rowData: any[];
-  private rowGroupPanelShow = "always";
-  private rowSelection = "multiple"
+  dataSource: MatTableDataSource<any>;
+  displayedColumns = ['mht_id', 'name', 'center', 'mobile', 'totalscore', 'isactive'];
 
-  columnDefs = [
-    {
-      headerName: "No",
-      valueGetter: 'node.rowIndex',
-      headerCheckboxSelection: true,
-      checkboxSelection: true
-    },
-    {
-      headerName: "Mht Id",
-      field: "mht_id",
-      type: "numberColumn",
-      filter: "agNumberColumnFilter"
-    },
-    {
-      headerName: "Name",
-      field: "name",
-      type: 'text',
-      filter: "agTextColumnFilter",
-    },
-    {
-      headerName: "Mobile",
-      field: "mobile",
-      type: 'text',
-      filter: "agTextColumnFilter"
-    },
-    {
-      headerName: "Center",
-      field: "center",
-      type: 'text',
-      filter: "agTextColumnFilter",
-      enableRowGroup: true
-    },
-    {
-      headerName: "Total Score",
-      field: "totalscore",
-      type: "numberColumn",
-      filter: "agNumberColumnFilter",
-      enableRowGroup: true
-    },
-    {
-      headerName: "Lives",
-      field: "lives",
-      type: "numberColumn",
-      filter: "agNumberColumnFilter",
-      enableRowGroup: true
-    },
-    {
-      headerName: "User Group",
-      field: "user_group",
-      type: 'text',
-      filter: "agTextColumnFilter",
-      enableRowGroup: true
-    }
-  ];
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
-  autoGroupColumnDef = {
-    headerName: "Group",
-    width: 200,
-    field: "mht_id",
-    valueGetter: function (params) {
-      if (params.node.group) {
-        return params.node.key;
-      } else {
-        return params.data[params.colDef.field];
-      }
-    },
-    headerCheckboxSelection: true,
-    cellRenderer: "agGroupCellRenderer",
-    cellRendererParams: { checkbox: true }
-  };
-
-  defaultColDef = {
-    editable: false,
-    enableRowGroup: true,
-    enablePivot: true,
-    enableValue: true,
-    sortable: true,
-    resizable: true,
-    filter: true
-  };
-
-  defaultColGroupDef = { marryChildren: true };
-
-  columnTypes = {
-    numberColumn: {
-      width: 83,
-      filter: "agNumberColumnFilter"
-    },
-    medalColumn: {
-      width: 100,
-      columnGroupShow: "open",
-      filter: false
-    },
-    nonEditableColumn: { editable: false }
-  };
+  @ViewChild(MatSort)
+  sort: MatSort;
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.getUserData();
   }
 
-  onGridReady(params) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-
-    this.api.getUsers().subscribe((data: any) => {
-      console.log('Level Data :: ', data.data.users);
-      this.rowData = data.data.users;
+  getUserData() {
+    this.api.getUsers().subscribe(data => {
+      console.log('USERS ::: ** ::: ', data.data.users);
+      this.dataSource = new MatTableDataSource(data.data.users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
+
+// bonus: 765
+// center: "Simandhar City"
+// createdAt: "2019-03-09T16:42:41.037Z"
+// email: "mohit.niruma@gmail.com"
+// fb_token: null
+// isactive: true
+// lives: 1
+// mht_id: 29077
+// mobile: "9924347186"
+// name: "Mohit Agravat"
+// onesignal_token: null
+// question_id: 880
+// totalscore: 1700
+// updatedAt: "2019-03-09T16:42:41.037Z"
+// user_group: "MBA"
+
+
+const DATA = [
+  {
+    'id': '1',
+    'name': 'Printed Dress',
+    'handle': 'printed-dress',
+    'description': 'Officia amet eiusmod eu sunt tempor voluptate laboris velit nisi amet enim proident et. Consequat laborum non eiusmod cillum eu exercitation. Qui adipisicing est fugiat eiusmod esse. Sint aliqua cupidatat pariatur mollit ad est proident reprehenderit. Eiusmod adipisicing laborum incididunt sit aliqua ullamco.',
+    'categories': [
+      'Dresses',
+      'Women'
+    ],
+    'tags': [
+      'dress',
+      'printed'
+    ],
+    'images': [
+      {
+        'id': 1,
+        'url': 'assets/images/ecommerce/product-image-placeholder.png',
+        'type': 'image'
+      },
+      {
+        'id': 2,
+        'url': 'assets/images/etc/air-balloons.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 3,
+        'url': 'assets/images/etc/avenue.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 4,
+        'url': 'assets/images/etc/cactus.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 5,
+        'url': 'assets/images/etc/early-sunrise.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 6,
+        'url': 'assets/images/etc/mountain-lake.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 7,
+        'url': 'assets/images/etc/road-trip.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 8,
+        'url': 'assets/images/etc/snow.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 9,
+        'url': 'assets/images/etc/tropical-beach.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 10,
+        'url': 'assets/images/etc/mountain-sunset.jpg',
+        'type': 'image'
+      }
+    ],
+    'priceTaxExcl': 9.309,
+    'priceTaxIncl': 10.24,
+    'taxRate': 10,
+    'comparedPrice': 19.90,
+    'quantity': 3,
+    'sku': 'A445BV',
+    'width': '22cm',
+    'height': '24cm',
+    'depth': '15cm',
+    'weight': '3kg',
+    'extraShippingFee': 3.00,
+    'active': true
+  },
+  {
+    'id': '2',
+    'name': 'Green Skirt',
+    'handle': 'green-skirt',
+    'description': 'Duis anim est non exercitation consequat. Ullamco ut ipsum dolore est elit est ea elit ad fugiat exercitation. Adipisicing eu ad sit culpa sint. Minim irure Lorem eiusmod minim nisi sit est consectetur.',
+    'categories': [
+      'Skirts',
+      'Women'
+    ],
+    'tags': [
+      'dress',
+      'printed'
+    ],
+    'images': [
+      {
+        'id': 1,
+        'url': 'assets/images/ecommerce/product-image-placeholder.png',
+        'type': 'image'
+      },
+      {
+        'id': 2,
+        'url': 'assets/images/etc/air-balloons.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 3,
+        'url': 'assets/images/etc/avenue.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 4,
+        'url': 'assets/images/etc/cactus.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 5,
+        'url': 'assets/images/etc/early-sunrise.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 6,
+        'url': 'assets/images/etc/mountain-lake.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 7,
+        'url': 'assets/images/etc/road-trip.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 8,
+        'url': 'assets/images/etc/snow.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 9,
+        'url': 'assets/images/etc/tropical-beach.jpg',
+        'type': 'image'
+      },
+      {
+        'id': 10,
+        'url': 'assets/images/etc/mountain-sunset.jpg',
+        'type': 'image'
+      }
+    ],
+    'priceTaxExcl': 22.381,
+    'priceTaxIncl': 24.62,
+    'taxRate': 10,
+    'comparedPrice': 29.90,
+    'quantity': 92,
+    'sku': 'A445BV',
+    'width': '22cm',
+    'height': '24cm',
+    'depth': '15cm',
+    'weight': '3kg',
+    'extraShippingFee': 3.00,
+    'active': true
+  }
+];
--- src/app/main/user/list/list.module.ts
+++ src/app/main/user/list/list.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular';
+import { MatButtonModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule } from '@angular/material';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseWidgetModule } from '@fuse/components/widget/widget.module';
 
 const routes: Routes = [
     {
@@ -15,9 +19,25 @@ const routes: Routes = [
     imports: [
         CommonModule,
 
+        MatButtonModule,
+        MatChipsModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSortModule,
+        MatSnackBarModule,
+        MatTableModule,
+        MatTabsModule,
+
         RouterModule.forChild(routes),
 
         AgGridModule.withComponents([]),
+
+        FuseSharedModule,
+        FuseWidgetModule,
 
     ],
     declarations: [
